4d445368e766a8368595584116c7ba8e256f1b4e
Geordan <gcout006@uottawa.ca>
2021-11-08 22:53:35 -0500

    created the Product class, its constructors, as well as its getters and setters.

---
+++ app/src/main/java/com/example/lab4/Product.java
@@ -0,0 +1,32 @@
+package com.example.lab4;
+
+public class Product {
+    private int id;
+    private String productName;
+    private double price;
+
+    // constructors
+    public Product() {
+
+    }
+
+    public Product(int id, String productName, double price) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public Product (String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+
+    // setters and getters
+    public void setID(int id) { this.id = id; }
+    public int getId() { return this.id; }
+    public void setProductName(String productName) { this.productName = productName; }
+    public String getProductName() {return this.productName; }
+    public void setPrice(double price) { this.price = price; }
+    public double getPrice() { return this.price; }
+}
